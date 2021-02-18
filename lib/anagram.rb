4d92dfee81dc7ae92ee827c948b8b56b2afe8159
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-02-18 11:53:06 -0500

    commit tree efcb30b399ccf565b73d31ee62ac6d1925462f5a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+def initialize(word)
+@word = word
+end
+
+attr_accessor :word
+
+def match(array)
+array.keep_if {|element| element.chars.sort == @word.chars.sort}
+end
+end
